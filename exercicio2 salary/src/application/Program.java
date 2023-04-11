51390f16ddffda8efa81cbda6f7a8c2c9f40f938
avinn <avinn@Vinny-w11>
2023-04-11 01:37:19 -0300

    removendo IF e utilizando objeto corretamente.

--- exercicio2 salary/src/application/Program.java
+++ exercicio2 salary/src/application/Program.java
@@ -8,28 +8,32 @@ import entities.Employee;
 public class Program {
 
 	public static void main(String[] args) {
-	
-	Locale.setDefault(Locale.US);
-	Scanner sc = new Scanner(System.in);
-	
-	Employee employee = new Employee();
-	
-	System.out.println("Enter employee name: ");
-	employee.name = sc.nextLine();
-	System.out.println("Enter gross salary: ");
-	employee.grossSalary = sc.nextDouble();
-	System.out.println("Enter tax: ");
-	employee.tax = sc.nextDouble();
-	
-	System.out.println("Employee: " + employee);
-	
-	System.out.print("Which percentage to increase salary? " );
-	employee.percentage = sc.nextDouble();
-	
-	
-	System.out.println("Update data: " + employee);
-	
-	sc.close();
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee employee = new Employee();
+		
+		System.out.print("Enter employee name: ");
+		employee.name = sc.nextLine();
+		
+		System.out.print("Enter gross salary: ");
+		employee.grossSalary = sc.nextDouble();
+		
+		System.out.print("Enter tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("Employee: " + employee);
+		System.out.println();
+		
+		System.out.print("Which percentage to increase salary? ");
+		double percentage = sc.nextDouble();
+		employee.increaseSalary(percentage);
+		System.out.println();
+		System.out.println("Updated data: " + employee);
+		
+		sc.close();
 		
 	}
 
--- exercicio2 salary/src/entities/Employee.java
+++ exercicio2 salary/src/entities/Employee.java
@@ -5,25 +5,17 @@ public class Employee {
 	public String name;
 	public double grossSalary;
 	public double tax;
-	public double percentage;
-			
+	
 	public double netSalary() {
-		if (percentage <=0) {
-		return grossSalary - tax;
-		}
-		else { 
-		return  grossSalary /100 * percentage + grossSalary-tax; 
-		}
+		return this.grossSalary - this.tax;
+	}
+	
+	public void increaseSalary(double percentage) {
+		this.grossSalary += this.grossSalary * percentage / 100;
 	}
 
-	public void increaseSalary (double percentage) {
-	percentage = (grossSalary*100) / percentage; 
-	}
-
+	
 	public String toString() {
-		return name
-		+ ", $ "
-		+ String.format("%.2f", this.netSalary());
+		return name + ", $ " + String.format("%.2f", netSalary());
 	}
-
 }
